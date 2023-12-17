58987ec7524d53d24ac5aa9d535a20d34d0b30e5
mboekhold <miguel_boekhold@hotmail.com>
2023-12-17 16:56:38 +0100

    fix styling issues

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <nav class="border-b border-b-gray-600 dark:bg-gray-900">
+        <nav class="border-b border-b-gray-600 ">
             <div class="flex flex-wrap items-center justify-between px-10 py-4">
                 <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="../assets/logo.png" class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dominos</span>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Dominos</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -18,16 +18,16 @@
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul
-                        class="font-medium flex flex-col p-2 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        class="font-medium flex flex-col p-2 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                                class="block py-2 px-3 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
                                 aria-current="page">Home
                             </a>
                         </li>
                         <li>
                             <a href="/practice"
-                                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                                class="block py-2 px-3 bg-blue-700 rounded md:bg-transparent text-blue-700 md:p-0"
                                 aria-current="page">Practice
                             </a>
                         </li>
