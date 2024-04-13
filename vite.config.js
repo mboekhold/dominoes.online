31f21c791b6a34104d5fdebee9f56b21230dd337
mboekhold <miguel_boekhold@hotmail.com>
2024-04-13 10:59:18 +0200

    check if makes difference

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/mboekhold.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
