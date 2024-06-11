3645a311cf000046256fa1f6022ab3af2b0098e7
mboekhold <miguel_boekhold@hotmail.com>
2024-06-11 21:26:50 +0200

    forgot animation for last chev

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -67,9 +67,9 @@
         <div v-else-if="player.id === 4" :class="'playerBoxWrapper' + player.id">
             <div :class="'playerBox' + player.id" class="flex flex-col -left-[80px] sm:left-0">
                 <div class="sm:hidden open-playerbox left-[75px] top-1/2 transform -translate-y-1/2 cursor-pointer w-8 h-10 flex justify-center items-center"
-                    :class="{ 'rotate-180': openPlayerBoxId === player.id }" @click="togglePlayerBox(player.id)">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor">
+                    @click="togglePlayerBox(player.id)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="toggle-icon"
+                        stroke="currentColor" :class="{ 'rotate-180': openPlayerBoxId === player.id }">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                     </svg>
 
