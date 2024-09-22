68f54fec5d5c0e242f46bf1c8fbeadc5722dbaef
mboekhold <miguel_boekhold@hotmail.com>
2024-09-22 16:13:41 +0200

    wip

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -27,6 +27,16 @@
                     Home
                 </div>
             </div>
+            <div class="mt-4 px-6 py-4 flex hover:cursor-pointer hover:text-white hover:bg-gray-700" @click="goHome()">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-7 w-7">
+                        <path fill-rule="evenodd" d="M5.166 2.621v.858c-1.035.148-2.059.33-3.071.543a.75.75 0 0 0-.584.859 6.753 6.753 0 0 0 6.138 5.6 6.73 6.73 0 0 0 2.743 1.346A6.707 6.707 0 0 1 9.279 15H8.54c-1.036 0-1.875.84-1.875 1.875V19.5h-.75a2.25 2.25 0 0 0-2.25 2.25c0 .414.336.75.75.75h15a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-2.25-2.25h-.75v-2.625c0-1.036-.84-1.875-1.875-1.875h-.739a6.706 6.706 0 0 1-1.112-3.173 6.73 6.73 0 0 0 2.743-1.347 6.753 6.753 0 0 0 6.139-5.6.75.75 0 0 0-.585-.858 47.077 47.077 0 0 0-3.07-.543V2.62a.75.75 0 0 0-.658-.744 49.22 49.22 0 0 0-6.093-.377c-2.063 0-4.096.128-6.093.377a.75.75 0 0 0-.657.744Zm0 2.629c0 1.196.312 2.32.857 3.294A5.266 5.266 0 0 1 3.16 5.337a45.6 45.6 0 0 1 2.006-.343v.256Zm13.5 0v-.256c.674.1 1.343.214 2.006.343a5.265 5.265 0 0 1-2.863 3.207 6.72 6.72 0 0 0 .857-3.294Z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="ml-3" :class="{'hidden': !showText}">
+                    Leaderboard
+                </div>
+            </div>
         </div>
         <div class="mt-auto">
             <div class="w-full h-[1px] bg-gray-700">
@@ -43,8 +53,8 @@
                     </div>
                 </div>
             </div>
-            <div v-else>
-                <div v-if="isAuthenticated" class="p-6 flex w-full cursor-pointer hover:text-white" @click="goProfile()">
+            <div v-else class="p-6">
+                <div v-if="isAuthenticated" class="flex w-full cursor-pointer hover:text-white" @click="goProfile()">
                     <div class="h-10 min-w-10 border border-gray-600 flex text-center rounded-md items-center justify-center">
                         <div class="text-gray-300">
                             {{ getUserFirstLetter() }}
@@ -59,14 +69,16 @@
                         </div>
                     </div>
                 </div>
-                <div v-else @click="goLogin()" class="bg-blue-600 rounded-md h-10 w-10 flex items-center justify-center hover:bg-blue-500 hover:cursor-pointer" :class="{'w-full': expand}">
-                    <div :class="{'hidden': expand }">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-gray-200">
-                            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
-                          </svg>
-                    </div>
-                    <div class="text-gray-100" :class="{'hidden': !expand }">
-                        Login
+                <div v-else>
+                    <div @click="goLogin()" class="bg-blue-600 rounded-md h-10 w-10 flex items-center justify-center hover:bg-blue-500 hover:cursor-pointer" :class="{'w-full': expand}">
+                        <div :class="{'hidden': this.showText }">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-gray-200">
+                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                              </svg>
+                        </div>
+                        <div class="text-gray-100" :class="{'hidden': !this.showText }">
+                            Login
+                        </div>
                     </div>
                 </div>
                             
