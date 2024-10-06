e141fa46f0e141e37ea474eb6b1079942029abef
mboekhold <miguel_boekhold@hotmail.com>
2024-10-06 10:42:23 +0200

    added leaderboard component

---
+++ src/views/LeaderboardView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="ml-20 px-20 py-5 w-1/2 text-white">
+        <Leaderboard />
+    </div>
+</template>
+<script>
+import Leaderboard from '@/components/Leaderboard.vue';
+export default {
+    components: {
+        Leaderboard
+    }
+}
+</script>
+<style>
+    
+</style>
