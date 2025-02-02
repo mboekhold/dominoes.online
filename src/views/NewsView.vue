d7adb61beebc46091b1e88175030efe5e81156d8
mboekhold <miguel_boekhold@hotmail.com>
2025-02-02 16:55:25 +0100

    add news section

---
+++ src/views/NewsView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="lg:ml-20 p-2 mt-10 lg:px-48 py-5 text-white">
+        News
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
